6e70c0b5fe4aea2340aa7392e3589406559b2e9b
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-10-30 11:49:50 +0100

    code

---
+++ Text/PrettyPrint/Compact.hs
@@ -0,0 +1,479 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Text.PrettyPrint.Compact where
+
+import Control.Applicative
+import Data.Monoid
+
+import Text.PrettyPrint.Compact.Core as Text.PrettyPrint.Compact
+
+type Indentation = Int
+
+infixr 5 </>,<//>,<$$$>,<$$>
+infixr 6 <+>
+
+-- | The document @(list xs)@ comma separates the documents @xs@ and
+-- encloses them in square brackets. The documents are rendered
+-- horizontally if that fits the page. Otherwise they are aligned
+-- vertically. All comma separators are put in front of the elements.
+list :: [Doc] -> Doc
+list            = encloseSep lbracket rbracket comma
+
+-- | The document @(tupled xs)@ comma separates the documents @xs@ and
+-- encloses them in parenthesis. The documents are rendered
+-- horizontally if that fits the page. Otherwise they are aligned
+-- vertically. All comma separators are put in front of the elements.
+tupled :: [Doc] -> Doc
+tupled          = encloseSep lparen   rparen  comma
+
+
+-- | The document @(semiBraces xs)@ separates the documents @xs@ with
+-- semi colons and encloses them in braces. The documents are rendered
+-- horizontally if that fits the page. Otherwise they are aligned
+-- vertically. All semi colons are put in front of the elements.
+semiBraces :: [Doc] -> Doc
+semiBraces      = encloseSep lbrace   rbrace  semi
+
+-- | The document @(enclosure l r sep xs)@ concatenates the documents
+-- @xs@ separated by @sep@ and encloses the resulting document by @l@
+-- and @r@. The documents are rendered horizontally if that fits the
+-- page. Otherwise they are aligned vertically. All separators are put
+-- in front of the elements. For example, the combinator 'list' can be
+-- defined with @enclosure@:
+--
+-- > list xs = enclosure lbracket rbracket comma xs
+-- > test    = text "list" <+> (list (map int [10,200,3000]))
+--
+-- Which is layed out with a page width of 20 as:
+--
+-- @
+-- list [10,200,3000]
+-- @
+--
+-- But when the page width is 15, it is layed out as:
+--
+-- @
+-- list [10
+--      ,200
+--      ,3000]
+-- @
+encloseSep :: Doc -> Doc -> Doc -> [Doc] -> Doc
+encloseSep left right sep ds
+    = case ds of
+        []  -> left <> right
+        [d] -> left <> d <> right
+        _   -> align (cat (zipWith (<>) (left : repeat sep) ds) <> right)
+
+-----------------------------------------------------------
+-- punctuate p [d1,d2,...,dn] => [d1 <> p,d2 <> p, ... ,dn]
+-----------------------------------------------------------
+
+
+-- | @(punctuate p xs)@ concatenates all documents in @xs@ with
+-- document @p@ except for the last document.
+--
+-- > someText = map text ["words","in","a","tuple"]
+-- > test     = parens (align (cat (punctuate comma someText)))
+--
+-- This is layed out on a page width of 20 as:
+--
+-- @
+-- (words,in,a,tuple)
+-- @
+--
+-- But when the page width is 15, it is layed out as:
+--
+-- @
+-- (words,
+--  in,
+--  a,
+--  tuple)
+-- @
+--
+-- (If you want put the commas in front of their elements instead of
+-- at the end, you should use 'tupled' or, in general, 'encloseSep'.)
+punctuate :: Doc -> [Doc] -> [Doc]
+punctuate p []      = []
+punctuate p [d]     = [d]
+punctuate p (d:ds)  = (d <> p) : punctuate p ds
+
+
+-----------------------------------------------------------
+-- high-level combinators
+-----------------------------------------------------------
+
+
+-- | The document @(sep xs)@ concatenates all documents @xs@ either
+-- horizontally with @(\<+\>)@, if it fits the page, or vertically with
+-- @(\<$\>)@.
+--
+-- > sep xs  = group (vsep xs)
+sep :: [Doc] -> Doc
+sep             = group . vsep
+
+-- | The document @(fillSep xs)@ concatenates documents @xs@
+-- horizontally with @(\<+\>)@ as long as its fits the page, than
+-- inserts a @line@ and continues doing that for all documents in
+-- @xs@.
+--
+-- > fillSep xs  = foldr (\<\/\>) empty xs
+fillSep :: [Doc] -> Doc
+fillSep         = foldDoc (</>)
+
+-- | The document @(hsep xs)@ concatenates all documents @xs@
+-- horizontally with @(\<+\>)@.
+hsep :: [Doc] -> Doc
+hsep            = foldDoc (<+>)
+
+
+-- | The document @(vsep xs)@ concatenates all documents @xs@
+-- vertically with @(\<$\>)@. If a 'group' undoes the line breaks
+-- inserted by @vsep@, all documents are separated with a space.
+--
+-- > someText = map text (words ("text to lay out"))
+-- >
+-- > test     = text "some" <+> vsep someText
+--
+-- This is layed out as:
+--
+-- @
+-- some text
+-- to
+-- lay
+-- out
+-- @
+--
+-- The 'align' combinator can be used to align the documents under
+-- their first element
+--
+-- > test     = text "some" <+> align (vsep someText)
+--
+-- Which is printed as:
+--
+-- @
+-- some text
+--      to
+--      lay
+--      out
+-- @
+vsep :: [Doc] -> Doc
+vsep            = foldDoc (<$$$>)
+
+-- | The document @(cat xs)@ concatenates all documents @xs@ either
+-- horizontally with @(\<\>)@, if it fits the page, or vertically with
+-- @(\<$$\>)@.
+--
+-- > cat xs  = group (vcat xs)
+cat :: [Doc] -> Doc
+cat             = group . vcat
+
+-- | The document @(fillCat xs)@ concatenates documents @xs@
+-- horizontally with @(\<\>)@ as long as its fits the page, than inserts
+-- a @linebreak@ and continues doing that for all documents in @xs@.
+--
+-- > fillCat xs  = foldr (\<\/\/\>) empty xs
+fillCat :: [Doc] -> Doc
+fillCat         = foldDoc (<//>)
+
+-- | The document @(hcat xs)@ concatenates all documents @xs@
+-- horizontally with @(\<\>)@.
+hcat :: [Doc] -> Doc
+hcat            = foldDoc (<>)
+
+-- | The document @(vcat xs)@ concatenates all documents @xs@
+-- vertically with @(\<$$\>)@. If a 'group' undoes the line breaks
+-- inserted by @vcat@, all documents are directly concatenated.
+vcat :: [Doc] -> Doc
+vcat            = foldDoc (<$$>)
+
+foldDoc :: (Doc -> Doc -> Doc) -> [Doc] -> Doc
+foldDoc f []       = mempty
+foldDoc f ds       = foldr1 f ds
+
+-- | The document @(x \<+\> y)@ concatenates document @x@ and @y@ with a
+-- @space@ in between.  (infixr 6)
+(<+>) :: Doc -> Doc -> Doc
+x <+> y         = x <> space <> y
+
+-- | The document @(x \<\/\> y)@ concatenates document @x@ and @y@ with a
+-- 'softline' in between. This effectively puts @x@ and @y@ either
+-- next to each other (with a @space@ in between) or underneath each
+-- other. (infixr 5)
+(</>) :: Doc -> Doc -> Doc
+x </> y         = x <> softline <> y
+
+-- | The document @(x \<\/\/\> y)@ concatenates document @x@ and @y@ with
+-- a 'softbreak' in between. This effectively puts @x@ and @y@ either
+-- right next to each other or underneath each other. (infixr 5)
+(<//>) :: Doc -> Doc -> Doc
+x <//> y        = x <> softbreak <> y
+
+-- | The document @(x \<$\> y)@ concatenates document @x@ and @y@ with a
+-- 'line' in between. (infixr 5)
+(<$$$>) :: Doc -> Doc -> Doc
+x <$$$> y         = x <> line <> y
+
+-- | The document @(x \<$$\> y)@ concatenates document @x@ and @y@ with
+-- a @linebreak@ in between. (infixr 5)
+(<$$>) :: Doc -> Doc -> Doc
+x <$$> y        = x <> linebreak <> y
+
+-- | The document @softline@ behaves like 'space' if the resulting
+-- output fits the page, otherwise it behaves like 'line'.
+--
+-- > softline = group line
+softline :: Doc
+softline        = group line
+
+-- | The document @softbreak@ behaves like 'empty' if the resulting
+-- output fits the page, otherwise it behaves like 'line'.
+--
+-- > softbreak  = group linebreak
+softbreak :: Doc
+softbreak       = group linebreak
+
+-- | Document @(squotes x)@ encloses document @x@ with single quotes
+-- \"'\".
+squotes :: Doc -> Doc
+squotes         = enclose squote squote
+
+-- | Document @(dquotes x)@ encloses document @x@ with double quotes
+-- '\"'.
+dquotes :: Doc -> Doc
+dquotes         = enclose dquote dquote
+
+-- | Document @(braces x)@ encloses document @x@ in braces, \"{\" and
+-- \"}\".
+braces :: Doc -> Doc
+braces          = enclose lbrace rbrace
+
+-- | Document @(parens x)@ encloses document @x@ in parenthesis, \"(\"
+-- and \")\".
+parens :: Doc -> Doc
+parens          = enclose lparen rparen
+
+-- | Document @(angles x)@ encloses document @x@ in angles, \"\<\" and
+-- \"\>\".
+angles :: Doc -> Doc
+angles          = enclose langle rangle
+
+-- | Document @(brackets x)@ encloses document @x@ in square brackets,
+-- \"[\" and \"]\".
+brackets :: Doc -> Doc
+brackets        = enclose lbracket rbracket
+
+-- | The document @(enclose l r x)@ encloses document @x@ between
+-- documents @l@ and @r@ using @(\<\>)@.
+enclose :: Doc -> Doc -> Doc -> Doc
+enclose l r x   = l <> x <> r
+
+char :: Char -> Doc
+char x = text [x]
+
+-- | The document @lparen@ contains a left parenthesis, \"(\".
+lparen :: Doc
+lparen          = char '('
+-- | The document @rparen@ contains a right parenthesis, \")\".
+rparen :: Doc
+rparen          = char ')'
+-- | The document @langle@ contains a left angle, \"\<\".
+langle :: Doc
+langle          = char '<'
+-- | The document @rangle@ contains a right angle, \">\".
+rangle :: Doc
+rangle          = char '>'
+-- | The document @lbrace@ contains a left brace, \"{\".
+lbrace :: Doc
+lbrace          = char '{'
+-- | The document @rbrace@ contains a right brace, \"}\".
+rbrace :: Doc
+rbrace          = char '}'
+-- | The document @lbracket@ contains a left square bracket, \"[\".
+lbracket :: Doc
+lbracket        = char '['
+-- | The document @rbracket@ contains a right square bracket, \"]\".
+rbracket :: Doc
+rbracket        = char ']'
+
+
+-- | The document @squote@ contains a single quote, \"'\".
+squote :: Doc
+squote          = char '\''
+-- | The document @dquote@ contains a double quote, '\"'.
+dquote :: Doc
+dquote          = char '"'
+-- | The document @semi@ contains a semi colon, \";\".
+semi :: Doc
+semi            = char ';'
+-- | The document @colon@ contains a colon, \":\".
+colon :: Doc
+colon           = char ':'
+-- | The document @comma@ contains a comma, \",\".
+comma :: Doc
+comma           = char ','
+
+-- | The document @dot@ contains a single dot, \".\".
+dot :: Doc
+dot             = char '.'
+-- | The document @backslash@ contains a back slash, \"\\\".
+backslash :: Doc
+backslash       = char '\\'
+-- | The document @equals@ contains an equal sign, \"=\".
+equals :: Doc
+equals          = char '='
+
+
+-----------------------------------------------------------
+-- semi primitive: fill and fillBreak
+-----------------------------------------------------------
+
+-- | The document @(fillBreak i x)@ first renders document @x@. It
+-- than appends @space@s until the width is equal to @i@. If the
+-- width of @x@ is already larger than @i@, the nesting level is
+-- increased by @i@ and a @line@ is appended. When we redefine @ptype@
+-- in the previous example to use @fillBreak@, we get a useful
+-- variation of the previous output:
+--
+-- > ptype (name,tp)
+-- >        = fillBreak 6 (text name) <+> text "::" <+> text tp
+--
+-- The output will now be:
+--
+-- @
+-- let empty  :: Doc
+--     nest   :: Indentation -> Doc -> Doc
+--     linebreak
+--            :: Doc
+-- @
+fillBreak :: Indentation -> Doc -> Doc
+fillBreak f x   = width x (\w ->
+                  if (w > f) then nest f linebreak
+                             else spacing (f - w))
+
+
+-- | The document @(fill i x)@ renders document @x@. It than appends
+-- @space@s until the width is equal to @i@. If the width of @x@ is
+-- already larger, nothing is appended. This combinator is quite
+-- useful in practice to output a list of bindings. The following
+-- example demonstrates this.
+--
+-- > types  = [("empty","Doc")
+-- >          ,("nest","Indentation -> Doc -> Doc")
+-- >          ,("linebreak","Doc")]
+-- >
+-- > ptype (name,tp)
+-- >        = fill 6 (text name) <+> text "::" <+> text tp
+-- >
+-- > test   = text "let" <+> align (vcat (map ptype types))
+--
+-- Which is layed out as:
+--
+-- @
+-- let empty  :: Doc
+--     nest   :: Indentation -> Doc -> Doc
+--     linebreak :: Doc
+-- @
+fill :: Indentation -> Doc -> Doc
+fill f d        = width d (\w ->
+                  if (w >= f) then mempty
+                              else spacing (f - w))
+
+width :: Doc -> (Indentation -> Doc) -> Doc
+width d f       = column (\k1 -> d <> column (\k2 -> f (k2 - k1)))
+
+
+-----------------------------------------------------------
+-- semi primitive: Alignment and indentation
+-----------------------------------------------------------
+
+-- | The document @(indent i x)@ indents document @x@ with @i@ spaces.
+--
+-- > test  = indent 4 (fillSep (map text
+-- >         (words "the indent combinator indents these words !")))
+--
+-- Which lays out with a page width of 20 as:
+--
+-- @
+--     the indent
+--     combinator
+--     indents these
+--     words !
+-- @
+indent :: Indentation -> Doc -> Doc
+indent i d      = hang i (spacing i <> d)
+
+-- | The hang combinator implements hanging indentation. The document
+-- @(hang i x)@ renders document @x@ with a nesting level set to the
+-- current column plus @i@. The following example uses hanging
+-- indentation for some text:
+--
+-- > test  = hang 4 (fillSep (map text
+-- >         (words "the hang combinator indents these words !")))
+--
+-- Which lays out on a page with a width of 20 characters as:
+--
+-- @
+-- the hang combinator
+--     indents these
+--     words !
+-- @
+--
+-- The @hang@ combinator is implemented as:
+--
+-- > hang i x  = align (nest i x)
+hang :: Indentation -> Doc -> Doc
+hang i d        = align (nest i d)
+
+-- | The document @(align x)@ renders document @x@ with the nesting
+-- level set to the current column. It is used for example to
+-- implement 'hang'.
+--
+-- As an example, we will put a document right above another one,
+-- regardless of the current nesting level:
+--
+-- > x $$ y  = align (x <$$$> y)
+--
+-- > test    = text "hi" <+> (text "nice" $$ text "world")
+--
+-- which will be layed out as:
+--
+-- @
+-- hi nice
+--    world
+-- @
+align :: Doc -> Doc
+align d         = column (\k ->
+                  nesting (\i -> nest (k - i) d))   --nesting might be negative :-)
+
+
+-- | The @line@ document advances to the next line and indents to the
+-- current nesting level. Document @line@ behaves like @(text \" \")@
+-- if the line break is undone by 'group'.
+line :: Doc
+line            = Line False
+
+-- | The @linebreak@ document advances to the next line and indents to
+-- the current nesting level. Document @linebreak@ behaves like
+-- 'empty' if the line break is undone by 'group'.
+linebreak :: Doc
+linebreak       = Line True
+
+-- | The document @(nest i x)@ renders document @x@ with the current
+-- indentation level increased by i (See also 'hang', 'align' and
+-- 'indent').
+--
+-- > nest 2 (text "hello" <$$$> text "world") <$$$> text "!"
+--
+-- outputs as:
+--
+-- @
+-- hello
+--   world
+-- !
+-- @
+nest :: Indentation -> Doc -> Doc
+nest i x        = Nest i x
+
+column, nesting :: (Indentation -> Doc) -> Doc
+column f        = Column f
+nesting f       = Nesting f
---
+++ Text/PrettyPrint/Compact/Core.hs
@@ -0,0 +1,136 @@
+{-# LANGUAGE RecordWildCards #-}
+module Text.PrettyPrint.Compact.Core(Doc(..),pretty,group,flatten,space,spacing,text) where
+
+import Data.Monoid
+import Data.Function (on)
+import Data.List (partition,minimumBy,sort)
+import Data.Either (partitionEithers)
+
+type Indentation = Int
+
+data Box = Str String | Spacing Indentation | NewLine {-^ not allowed in the input -}
+
+
+data Doc = Empty
+         | Text Box
+         | Line !Bool            -- True <=> when undone by group, do not insert a space
+         | Cat Doc Doc
+         | Nest Indentation Doc
+         | Union Doc Doc           -- invariant: first lines of first doc longer than the first lines of the second doc
+         | Column  (Indentation -> Doc)
+         | Nesting (Indentation -> Doc)
+
+
+instance Monoid Doc where
+  mempty = Empty
+  mappend = Cat
+
+text :: String -> Doc
+text = Text . Str
+
+spacing = Text . Spacing
+
+space = spacing 1
+
+group :: Doc -> Doc
+group x         = Union (flatten x) x
+
+flatten :: Doc -> Doc
+flatten (Cat x y)       = Cat (flatten x) (flatten y)
+flatten (Nest i x)      = Nest i (flatten x)
+flatten (Line noSpace)  = if noSpace then Empty else space
+flatten (Union x _y)    = flatten x
+flatten (Column f)      = Column (flatten . f)
+flatten (Nesting f)     = Nesting (flatten . f)
+flatten other           = other                     --Empty,Char,Text
+
+len :: Box -> Int
+len (Str s) = length s
+len (Spacing x) = x
+len NewLine = 0
+
+data Docs   = Nil
+            | Cons !Indentation Doc Docs
+
+data Process = Process {overflow :: Indentation
+                       ,curIndent :: !Indentation -- current indentation
+                       ,numToks :: Int -- total number of non-space tokens produced
+                       ,tokens :: [Box] -- tokens produced, in reverse order
+                       ,rest :: !Docs -- rest of the input document to process
+                       }
+-- The ⟨filtering⟩ step takes all process states starting at the current
+-- line, and discards those that are dominated.  A process state
+-- dominates another one if it was able to produce more tokens while
+-- having less indentation. This is implemented by first sorting the
+-- process states by following 'measure', and then applying the
+-- 'filtering' function.
+
+measure :: Process -> (Indentation, Int)
+measure Process{..} = (curIndent, negate numToks)
+
+instance Eq Process where
+  (==) = (==) `on` measure
+instance Ord Process where
+  compare = compare `on` measure
+
+filtering :: [Process] -> [Process]
+filtering (x:y:xs) | numToks x >= numToks y = filtering (x:xs)
+                   | otherwise = x:filtering (y:xs)
+filtering xs = xs
+
+renderAll :: Indentation -> Indentation -> Doc -> [Box]
+renderAll rfrac w doc = reverse $ loop [Process 0 0 0 [] $ Cons 0 doc Nil]
+    where
+      loop ps = case dones of
+        ((_,done):_) -> done -- here we could attempt to do a better choice. Seems to be fine for now.
+        [] -> case conts of
+          (_:_) -> loop $ filtering $ sort $ conts -- See the comment ⟨filtering⟩ above
+          [] -> case conts'over of
+            (_:_) -> loop [minimumBy (compare `on` overflow) conts'over]
+            [] -> case dones'over of
+              ((_,done):_) -> done
+              [] -> [Str "Pretty print: Panic"]
+
+        where
+          -- advance all processes by one line (if possible)
+          ps' = concatMap (\Process{..} -> rall numToks tokens curIndent curIndent rest) ps
+          -- Have some processes reached the end? With some overflow?
+          (dones0,conts0) = partitionEithers ps'
+          (conts,conts'over) = partition (\p -> overflow p <= 0) conts0
+          (dones,dones'over) = partition (\(o,_) -> o <= 0) dones0
+
+      -- r :: the ribbon width in characters
+      r  = max 0 (min w (w * rfrac))
+
+      -- Automatically inserted spacing does not count as doing more production.
+      count (Spacing _) = 0
+      count (Str _) = 1
+
+      -- Compute the state(s) after reaching the end of line.
+      -- rall :: n = indentation of current line
+      --         k = current column
+      --        (ie. (k >= n) && (k - n == count of inserted characters)
+      rall :: Int -> [Box] -> Indentation -> Indentation -> Docs -> [Either (Indentation,[Box]) Process]
+      rall nts ts n k ds0 = case ds0 of
+         Nil ->  [Left $ (overflow,ts)] -- Done!
+         Cons i d ds -> case d of
+            Empty       -> rall nts ts n k ds
+            Text s      -> let k' = k+len s in seq k' (rall (nts+count s) (s:ts) n k' ds)
+            Line _      -> [Right $ Process overflow i nts (Spacing i:NewLine:ts) ds] -- "yield" when the end of line is reached
+            Cat x y     -> rall nts ts n k (Cons i x (Cons i y ds))
+            Nest j x    -> let i' = i+j in seq i' (rall nts ts n k (Cons i' x ds))
+            Union x y   -> rall nts ts n k (Cons i x ds) ++ rall nts ts n k (Cons i y ds)
+            Column f    -> rall nts ts n k (Cons i (f k) ds)
+            Nesting f   -> rall nts ts n k (Cons i (f i) ds)
+        where overflow = negate $ min (w - k) (r - k + n)
+
+
+layout :: [Box] -> String
+layout (Str s:xs) = s ++ layout xs
+layout (NewLine:xs) = '\n' : layout xs
+layout (Spacing x:xs) = replicate x ' ' ++ layout xs
+
+pretty :: Indentation -> Indentation -> Doc -> String
+pretty rfrac w d = do
+  layout $ renderAll rfrac w d
+
