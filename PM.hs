0203aa66523735cc95059bea4a1f5cdc88f2a453
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2015-07-27 21:37:01 +0200

    up to diagram

--- PM.hs
+++ PM.hs
@@ -20,8 +20,8 @@ preamble body = do
   newtheorem "principle" "Principle"
   env "document" body
 
-principle :: TeX -> Tex SortedLabel
-principle = deflike "Principle" "principle" mempty
+principle :: TeX -> TeX -> Tex SortedLabel
+principle title = deflike "Principle" "principle" title
 
 
 header :: Tex ()
@@ -50,17 +50,22 @@ mainText = «
 
 A pretty printing algorithm renders data structures in a way which
 makes them pleasant to read. (The data structures in question are
-often represent programs, but not always.) I propose that two
-principles are essential to pretty printing:
+often represent programs, but not always.) I propose the following
+three laws of pretty printing:
 
-@pcp_layout<-principle« A pretty printer should make clever use of layout, to make it easy
-             for a human to recognise the hierarchical organisation of data.»
+@pcp_visibility<-principle«Visibility»«Pretty printer shall
+layout all its output within the width of the page.»
 
-@pcp_space<-principle«A pretty printer should minimize the amount of
+@pcp_compact<-principle«compactness»«A pretty printer shall minimize the amount of
 space used to display the data.»
 
+@pcp_layout<-principle«layout»« A pretty printer shall make clever use of layout, to make it easy
+             for a human to recognise the hierarchical organisation of data.»
 
-Even though they are not stated as such, these principle guide classic
+Furthermore, the first law takes precedence over the second one, which itself takes precedence over the third.
+
+
+Even though they are not stated as such, these laws guide classic
 functional pretty printing libraries, such as Hughes' and Wadler's.
 Indeed, the functional programming community often uses pretty
 printing to showcase proper program design: Hughes' pretty printer
@@ -187,6 +192,212 @@ We have given a syntax for describing documents, but what should be
 its semantics?  What does it mean to pretty print a document? Technically,
 what is the specification of @hask«render»?
 
+Let us use an example to try and answer the question. Suppose we want
+to pretty print the following s-expr (which is specially crafted to
+demonstrate issues with both Hughes and Wadler libraries):
 
+> testData :: SExpr
+> testData = SExpr [SExpr [Atom "12345", abcd4],
+>                   SExpr [Atom "12345678", abcd4]]
+>   where abcd4 = SExpr [abcd,abcd,abcd,abcd]
+
+We would like elements inside an S-Expr to be either
+aligned vertically (for legibility, @pcp_layout),
+or concatenated horizontally (for compactness, @pcp_compact).
+The second option will be preferred over the first, as long
+as the text fits within the page width.
+
+Thus, printed on a 80-column-wide page, we'd like to get:
+
+@verbatim«
+12345678901234567890123456789012345678901234567890123456789012345678901234567890
+
+((12345 ((a b c d) (a b c d) (a b c d) (a b c d) (a b c d)))
+ (12345678 ((a b c d) (a b c d) (a b c d) (a b c d) (a b c d))))
+»
+
+(The first line is a helper showing the column of a given character.)
+Printed on a 20-column-wide page, we'd like to get:
+
+@verbatim«
+12345678901234567890
+
+((12345 ((a b c d)
+         (a b c d)
+         (a b c d)
+         (a b c d)
+         (a b c d)))
+ (12345678
+  ((a b c d)
+   (a b c d)
+   (a b c d)
+   (a b c d)
+   (a b c d))))
+»
+
+Unfortunately, in this case, and using Hughes' library, we would get the following output:
+
+@verbatim«
+12345678901234567890
+((12345 ((a b c d)
+         (a b c d)
+         (a b c d)
+         (a b c d)
+         (a b c d)))
+ (12345678 ((a
+             b
+             c
+             d)
+            (a
+             b
+             c
+             d)
+            (a
+             b
+             c
+             d)
+            (a
+             b
+             c
+             d)
+            (a
+             b
+             c
+             d))))
+»
+
+The above output uses way more space than
+necessary, violating @pcp_compact.  
+Why is that? Hughes states that @q«it would be
+unreasonably inefficient for a pretty-printer do decide whether or not
+to split the first line of a document on the basis of the content of
+the last.» (sec. 7.4 of his paper).  Therefore, he chooses a greedy
+algorithm, which tries to fit as much as possible on a single line,
+without regard for what comes next.  In our example, the algorithm
+fits @texttt«(12345678 ((a», but then it has committed to a very deep
+indentation level, which forces to display the remainder of the
+document in a narrow space.
+
+One may wonder what would happen with Wadler's library. Unfortunately,
+its API cannot even express the layout we are after! Indeed, one can
+only specify a @emph«constant» amount of indentation, not one that depends
+on the contents of a document.  This means that Wadler's library lacks
+the capability to express that a multi-line sub-document $b$ should be
+laid out to the right of a document $a$: $a$ must be put below $b$.
+Thus, with an appropriate specification, Wadler would render our
+example as follows:
+
+@verbatim«
+12345678901234567890
+((12345
+  ((a b c d)
+   (a b c d)
+   (a b c d)
+   (a b c d)
+   (a b c d)))
+ (12345678
+  ((a b c d)
+   (a b c d)
+   (a b c d)
+   (a b c d)
+   (a b c d))))
+»
+
+It's not too bad! But it inserts a spurious line break after the atom
+@texttt«12345678». While this may be acceptable to some, I find it
+disappointing for two reasons. First, spurious line breaks may appear
+in many situations. Second, the element which is rejected to a next
+can only be indented by a constant amount. Let us say we would like to
+pretty print the following ml-style snippet:
+@verbatim«
+Pattern = expression [listElement x,
+                      listElement y,
+                      listElement z,
+                      listElement w]
+»
+Wadler will force us to lay it out like so:
+@verbatim«
+Pattern = expression
+  [listElement x,
+   listElement y,
+   listElement z,
+   listElement w]
+»
+Aligning the argument of the expression below the equal sign is bad:
+it obscures the structure of the program.
+@pcp_layout is not respected. In fact, the lack of a combinator
+for alignment proved too constraining for Leijen. Indeed, in his
+implemenation of Wadler's design (which is a de-facto standard implementation) he
+an alignment combinator. But, also using a greedy algorithm, it
+necessarily suffers from the same issues as Hughes library.
+
+Because of the limitations of greediness, I propose to give up on it.
+In exchange for its lack of greediness,
+the library which I present in the rest of the paper
+guarantees to find the smallest output, while 
+retaining the ability to express vertical alignment of sub-documents.
+Yet, the final algorithm that I will arrive at is fast enough for use
+in common pretty printing tasks.
+
+@subsection«A Prettier API»
+
+Before getting into the problem of making a fast (enough)
+implementation, I'll refine the API for defining pretty layouts, and give 
+formal
+semantics for it. 
+
+Recall that we have inherited from Hughes a draft API:
+
+@haskell_«
+text  :: String -> d
+(<>)  :: Doc d => d -> d -> d
+($$)  :: Doc d => d -> d -> d
+»
+
+(I first ignore disjuction between
+possible layouts (`<|>`).)
+
+At this stage, classic functional pearls would state a number of laws
+that the above API has to satisfy, then infer a semantics from them.
+Fortunately, in our case, a straightforward interpretation for document
+works, so we can jump straight to the semantics and derive the laws from it.
+
+Let us interpret a layout as a non-empty list of lines to print. I'll
+simply use the type of lists and remember on the side the invariant.
+
+> type L = [String] -- non empty.
+
+> instance Layout L where
+
+Preparing a layout for printing is as easy as concatenation with
+newlines:
+
+>   render :: L -> String
+>   render = intercalate "\n"
+
+Embedding a string is thus immediate:
+
+>   text :: String -> L
+>   text s = [s]
+
+The interpretation of vertical concatenation ($$) requires barely more
+thought:
+
+<   ($$) :: L -> L -> L
+<   xs $$ ys = xs ++ ys
+
+What does horizontal concatenation (<>) mean? We will stick to Hughes'
+advice: "translate [to the right] the second operand, so that is tabs
+against the last character of the first operand". We can represent the
+situation diagramatically as follows:
+
+abstrLayout p0 h w lw = do
+  polygon [p0
+          ,p0+(w :- 0)
+          ,p0+(w :- h*lineHeight)
+          ,p0+(lw :- h*lineHeight)
+          ,p0+(lw :- (h+1)*lineHeight)
+          ,p0+(0 :- (h+1)*lineHeight)]
+  
 
 »
