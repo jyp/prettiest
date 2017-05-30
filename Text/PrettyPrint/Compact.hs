c7a607e2aad77576e00dc611064c6a035fe5a77b
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-05-31 00:27:13 +0300

    Add annotated s-expr example

--- Text/PrettyPrint/Compact.hs
+++ Text/PrettyPrint/Compact.hs
@@ -51,6 +51,17 @@
 --
 -- Yet, neither Hughes' nor Wadler's library can deliver those results.
 --
+-- === Annotations
+--
+-- For example we can annotate every /car/ element of S-Expressions,
+-- and in the rendering phase emphasise them by rendering them in uppercase.
+--
+-- >>> let pretty' :: SExpr -> Doc Any; pretty' (Atom s) = text s; pretty' (SExpr []) = text "()"; pretty' (SExpr (x:xs)) = text "(" <> (sep $ annotate (Any True) (pretty' x) : map pretty' xs) <> text ")"
+-- >>> let render' = annotatedRender (\a x -> if a == Just (Any True) then map toUpper x else x)
+-- >>> putStrLn $ render' $ pretty' testData
+-- ((ABCDE ((A B C D) (A B C D) (A B C D) (A B C D)))
+--  (ABCDEFGH ((A B C D) (A b c d) (A b c d) (A b c d))))
+--
 module Text.PrettyPrint.Compact (
    -- * Documents
    Doc,
@@ -450,3 +461,7 @@ spaces n = text $ replicate n ' '
 -- a linebreak in between. (infixr 5)
 ($$) :: Doc a -> Doc a -> Doc a
 ($$)  = (<$$>)
+
+-- $setup
+-- >>> import Data.Monoid
+-- >>> import Data.Char
