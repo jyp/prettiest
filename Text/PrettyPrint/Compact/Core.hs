e3ded2aa5ab8bd1a326b0ca4d8c1cef0d92e9a5b
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-05-30 13:05:37 +0300

    Add annotate

--- Text/PrettyPrint/Compact/Core.hs
+++ Text/PrettyPrint/Compact/Core.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables, TypeSynonymInstances, FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, ViewPatterns, DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
-module Text.PrettyPrint.Compact.Core(Layout(..),Document(..),Doc) where
+module Text.PrettyPrint.Compact.Core(Layout(..),Render(..),Document(..),Doc,annotate) where
 
 import Data.List (sort,groupBy,intercalate,minimumBy)
 import Data.Function (on)
@@ -15,6 +15,8 @@ data AS a = AS (Maybe a) String
 getASString :: AS a -> String
 getASString (AS _ s) = s
 
+
+
 -- | Make non-annotated 'AS'
 mkAS :: String -> AS a
 mkAS = AS Nothing
@@ -24,10 +26,10 @@ instance Semigroup a => Semigroup (AS a) where
     where
       f (Just x) (Just y) = Just (x <> y)
       f x        Nothing  = x
-      f Nothing  y        = y 
+      f Nothing  y        = y
 
 newtype L a = L (Seq (AS a)) -- non-empty sequence
-  deriving (Eq,Ord,Show,Functor)
+  deriving (Eq,Ord,Show,Functor,Foldable,Traversable)
 
 instance Semigroup a => Semigroup (L a) where
    L (viewr -> xs :> x) <> L (viewl -> y :< ys) = L (xs <> singleton (x <> y) <> fmap (indent <>) ys)
@@ -39,47 +41,56 @@ instance Semigroup a =>  Monoid (L a) where
    mappend = (<>)
 
 instance Semigroup a => Layout (L a) where
-   render (L xs) = intercalate "\n" $ map getASString $ toList xs
    text = L . singleton . mkAS
    flush (L xs) = L (xs |> mkAS "")
 
+instance Render L where
+  render f (L xs) = intercalate "\n" $ map f' $ toList xs
+    where
+      f' (AS Nothing s)  = s
+      f' (AS (Just a) s) = f a s
+
+class Render d where
+  render :: (a -> String -> String) -- ^ how to annotate the string. /Note:/ the annotation should preserve the visible length of the string.
+         -> d a                     -- ^ renderable
+         -> String
 
 class Monoid d => Layout d where
   text :: String -> d
   flush :: d -> d
-  render :: d -> String
+  -- render :: d -> String
 
 class Layout d => Document d where
   (<|>) :: d -> d -> d
 
-data M = M {height    :: Int,
-            lastWidth :: Int,
-            maxWidth  :: Int
-            }
-  deriving (Show,Eq,Ord)
+-- | type parameter is phantom.
+data M a = M {height    :: Int,
+              lastWidth :: Int,
+              maxWidth  :: Int
+              }
+  deriving (Show,Eq,Ord,Functor,Foldable,Traversable)
 
-instance Semigroup M where
+instance Semigroup (M a) where
   a <> b =
     M {maxWidth = max (maxWidth a) (maxWidth b + lastWidth a),
        height = height a + height b,
        lastWidth = lastWidth a + lastWidth b}
-  
-instance Monoid M where
+
+instance Monoid (M a) where
   mempty = text ""
   mappend = (<>)
 
-instance Layout M where
+instance Layout (M a) where
   text s = M {height = 0, maxWidth = length s, lastWidth = length s}
   flush a = M {maxWidth = maxWidth a,
                height = height a + 1,
                lastWidth = 0}
-  render = error "don't use render for M type"
 
 class Poset a where
   (≺) :: a -> a -> Bool
 
 
-instance Poset M where
+instance Poset (M a) where
   M c1 l1 s1 ≺ M c2 l2 s2 = c1 <= c2 && l1 <= l2 && s1 <= s2
 
 merge :: Ord a => [a] -> [a] -> [a]
@@ -107,7 +118,7 @@ pareto' acc (x:xs) = if any (≺ x) acc
                             -- is false. No need to refilter acc.
 
 
-newtype Doc a = MkDoc [(M,L a)] -- list sorted by lexicographic order for the first component
+newtype Doc a = MkDoc [(M a,L a)] -- list sorted by lexicographic order for the first component
   deriving Show
 
 instance (Ord a, Semigroup a) => Semigroup (Doc a) where
@@ -121,16 +132,18 @@ instance (Ord a, Semigroup a) => Monoid (Doc a) where
   mempty = text ""
   mappend = (<>)
 
-fits :: M -> Bool
+fits :: M a -> Bool
 fits x = maxWidth x <= 80
 
 instance (Ord a, Semigroup a) => Layout (Doc a) where
   flush (MkDoc xs) = MkDoc $ bests $ map sort $ groupBy ((==) `on` (height . fst)) $ (map flush xs)
   -- flush xs = pareto' [] $ sort $ (map flush xs)
   text s = MkDoc [text s]
-  render (MkDoc []) = error "No suitable layout found."
-  render (MkDoc xs@(x:_)) | maxWidth (fst x) <= 80 = render x
-                          | otherwise = render (minimumBy (compare `on` (maxWidth . fst)) xs)
+
+instance Render Doc where
+  render f (MkDoc []) = error "No suitable layout found."
+  render f (MkDoc xs@(x:_)) | maxWidth (fst x) <= 80 = render f (snd x)
+                            | otherwise = render f $ snd (minimumBy (compare `on` (maxWidth . fst)) xs)
 
 instance (Ord a, Semigroup a) => Document (Doc a) where
   MkDoc m1 <|> MkDoc m2 = MkDoc (bests [m1,m2])
@@ -139,7 +152,6 @@ instance (Ord a, Semigroup a) => Document (Doc a) where
 instance (Layout a, Layout b) => Layout (a,b) where
   text s = (text s, text s)
   flush (a,b) = (flush a, flush b)
-  render = render . snd
 
 instance (Document a, Document b) => Document (a,b) where
   (a,b) <|> (c,d) = (a<|>c,b<|>d)
@@ -149,3 +161,29 @@ instance (Poset a) => Poset (a,b) where
 
 instance (Ord a, Semigroup a) =>  IsString (Doc a) where
   fromString = text
+
+-- | `<>` new annotation to the 'Doc'.
+--
+-- Example: 'Any True' annotation will transform the rendered 'Doc' into uppercase:
+--
+-- >>> let r = putStrLn . render (\(Any b) x -> if b then map toUpper x else x)
+-- >>> r $ text "hello" <$$> annotate (Any True) (text "world")
+-- hello
+-- WORLD
+--
+annotate :: forall a. Semigroup a => a -> Doc a -> Doc a
+annotate a (MkDoc xs) = MkDoc (fmap (fmap annotateL) xs)
+  where
+    annotateL :: L a -> L a
+    annotateL (L s) = L (fmap annotateAS s)
+
+    annotateAS :: AS a -> AS a
+    annotateAS (AS Nothing s)  = AS ma s
+    annotateAS (AS (Just b) s) = AS (Just (b <> a)) s
+
+    ma = Just a
+
+-- $setup
+-- >>> import Text.PrettyPrint.Compact
+-- >>> import Data.Monoid
+-- >>> import Data.Char
